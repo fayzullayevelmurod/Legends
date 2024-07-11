b206c02e40985fcba9b575b7b0735a05a96ecb1e
SardorQobilov <sardorqobilov555@gmail.com>
2024-07-11 22:36:25 +0500

    personal-area_page finished

--- js/main.js
+++ js/main.js
@@ -1,58 +1,99 @@
 let swiper = new Swiper(".eventSwiper", {
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".event-button-next",
-        prevEl: ".event-button-prev",
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".event-button-next",
+    prevEl: ".event-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1.2,
     },
-    breakpoints: {
-        640: {
-            slidesPerView: 1.20,
-        },
-        993: {
-            slidesPerView: 3.10,
-        },
-        1200: {
-            slidesPerView: 4.20,
-        },
+    993: {
+      slidesPerView: 3.1,
     },
+    1200: {
+      slidesPerView: 4.2,
+    },
+  },
 });
-
 
 let swiper2 = new Swiper(".tarifSwiper", {
-    spaceBetween: 17,
-    loop: true,
-    navigation: {
-        nextEl: ".tarif_btn_next",
-        prevEl: ".tarif_btn_prev",
+  spaceBetween: 17,
+  loop: true,
+  navigation: {
+    nextEl: ".tarif_btn_next",
+    prevEl: ".tarif_btn_prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
     },
-    breakpoints: {
-        320: {
-            slidesPerView: 1.10,
-        },
-        993: {
-            slidesPerView: 2.75,
-        },
-        1200: {
-            slidesPerView: 3.74,
-        },
+    993: {
+      slidesPerView: 2.75,
     },
+    1200: {
+      slidesPerView: 3.74,
+    },
+  },
 });
 
 // accardion
-const accSingleTriggers = document.querySelectorAll('.js-acc-single-trigger');
+const accSingleTriggers = document.querySelectorAll(".js-acc-single-trigger");
 
-accSingleTriggers.forEach(trigger => trigger.addEventListener('click', toggleAccordion));
+accSingleTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", toggleAccordion)
+);
 
 function toggleAccordion() {
-    const items = document.querySelectorAll('.js-acc-item');
-    const thisItem = this.parentNode;
+  const items = document.querySelectorAll(".js-acc-item");
+  const thisItem = this.parentNode;
 
-    items.forEach(item => {
-        if (thisItem == item) {
-            thisItem.classList.toggle('is-open');
-            return;
-        }
-        item.classList.remove('is-open');
-    });
+  items.forEach((item) => {
+    if (thisItem == item) {
+      thisItem.classList.toggle("is-open");
+      return;
+    }
+    item.classList.remove("is-open");
+  });
 }
 // accardion
+
+// custom select
+try {
+  const selectBox = document.querySelectorAll(".select_box");
+
+  selectBox.forEach((item) => {
+    const select = item.querySelector(".custom_select");
+    let selectedText = item.querySelector(".selected_text");
+    const optionItem = item.querySelectorAll(".option_item");
+    const selectOption = item.querySelector(".select_option");
+
+    select.addEventListener("click", () => {
+      select.classList.toggle("active");
+      selectOption.classList.toggle("show");
+    });
+
+    optionItem.forEach((option) => {
+      option.addEventListener("click", () => {
+        selectedText.value = option.textContent;
+      });
+    });
+  });
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const img = input.nextElementSibling.querySelector("img");
+
+    if (input.type === "password") {
+      input.type = "text";
+      img.src = "images/icons/eye.svg"; // Change to eye icon
+    } else {
+      input.type = "password";
+      img.src = "images/icons/eye-slash.svg"; // Change to eye-slash icon
+    }
+  }
+} catch (error) {}
